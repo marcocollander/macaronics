bf949ce3a7e6f4c9e62c6fbc80949081fa3e07b3
Zenon Marek Raubuć <m.raubuc@wp.pl>
2024-04-19 00:13:53 +0200

    Add basic structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import {Layout} from "@/components";
+import {Toaster} from "react-hot-toast";
+
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}) {
+    return (
+        <Layout>
+            <Toaster/>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
